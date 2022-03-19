fe40fd23cd8e62bedf000a99074b376c9ed31cda
Teyeb <hussenab@gmail.com>
2022-03-19 10:54:20 +0200

    exit and enter

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,12 @@
-import React, { useEffect } from 'react'
-import { StyleSheet, StatusBar, View, Image, SafeAreaView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import {
+  StyleSheet,
+  StatusBar,
+  View,
+  Button,
+  TextInput,
+  SafeAreaView
+} from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 
 import tw from 'tailwind-react-native-classnames'
@@ -14,23 +21,62 @@ import MostSoldLists from './MostSoldLists'
 const HomeScreen = () => {
   const dispatch = useDispatch()
   const state = useSelector((state) => state.nav)
+  const [ans, setAns] = useState(false)
+  const [text, onChangeText] = React.useState('Useless Text')
+  const [number, onChangeNumber] = React.useState(null)
 
   //get user device token
   useEffect(() => {
     registerForPushNotificationsAsync().then((token) => {
       dispatch(setToken(token))
+      setAns(false)
     })
   }, [])
 
   return (
     <SafeAreaView style={[styles.container, tw`bg-white h-full`]}>
-      <NavBarTop />
-      <Tabs />
-      <MostSoldLists />
-
-      <View style={{ padding: 0 }}>
-        <MenuNavOptions />
-      </View>
+      {ans ? (
+        <>
+          <Button
+            title="close"
+            onPress={() => {
+              setAns(false)
+              onChangeText(null)
+              onChangeNumber(null)
+            }}
+          ></Button>
+          <NavBarTop />
+          <Tabs />
+          <MostSoldLists />
+          <View style={{ padding: 0 }}>
+            <MenuNavOptions />
+          </View>
+        </>
+      ) : (
+        <>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            placeholder="Enter text"
+            value={text}
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="Enter number"
+            keyboardType="numeric"
+          />
+          <Button
+            title="Enter "
+            onPress={() => {
+              setAns(true)
+              onChangeText(null)
+              onChangeNumber(null)
+            }}
+          ></Button>
+        </>
+      )}
     </SafeAreaView>
   )
 }
