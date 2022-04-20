53c2713176fa930cda2cb98a377c0115df8c3c8f
Teyeb <hussenab@gmail.com>
2022-04-21 00:36:44 +0300

    checkout page

--- screens/CheckoutScreen.js
+++ screens/CheckoutScreen.js
@@ -1,19 +1,32 @@
-import { StyleSheet, View, FlatList } from 'react-native'
+import { StyleSheet, View, FlatList, TouchableOpacity, Image } from 'react-native'
 import {Text} from 'react-native-elements'
 import React from 'react'
 import {useSelector} from 'react-redux'
+import tw from 'twrnc'
+
 
 const CheckoutScreen = () => {
   const cart = useSelector(state => state.nav.cart)
   return (
     <View>
-      <Text h4>Checkout Screen</Text>
+      <Text h4 style={tw`text-center text-purple-800`}>Checkout Screen</Text>
       <FlatList
           data={cart}
           keyExtractor={(item) => item.id}
           initialNumToRender={2}
-          numColumns={3}
-          renderItem={({ item }) => ( <Text>{item.name}</Text>)}
+          /* numColumns={2} */
+          renderItem={({ item }) => (
+          <TouchableOpacity style={tw`p-1 m-2`}>
+            
+            <Text style={tw`p-1 m-2 text-xl  text-cyan-600`}>{item.product.name}</Text>
+            <Image
+                  style={[
+                    tw` bg-white`,
+                    { height: 106, width: '45%',resizeMode: 'cover' }
+                  ]}
+                  source={{ uri: item.product.image }}
+                />
+          </TouchableOpacity>)}
       />
     </View>
   )
