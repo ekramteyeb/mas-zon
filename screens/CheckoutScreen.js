8cc05e864ceeaaa3dbce71dfad18645a3c168495
Teyeb <hussenab@gmail.com>
2022-04-20 23:57:54 +0300

    checkout page

--- screens/CheckoutScreen.js
+++ screens/CheckoutScreen.js
@@ -1,11 +1,20 @@
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, FlatList } from 'react-native'
 import {Text} from 'react-native-elements'
 import React from 'react'
+import {useSelector} from 'react-redux'
 
 const CheckoutScreen = () => {
+  const cart = useSelector(state => state.nav.cart)
   return (
     <View>
       <Text h4>Checkout Screen</Text>
+      <FlatList
+          data={cart}
+          keyExtractor={(item) => item.id}
+          initialNumToRender={2}
+          numColumns={3}
+          renderItem={({ item }) => ( <Text>{item.name}</Text>)}
+      />
     </View>
   )
 }
